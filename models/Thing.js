11cb654afc24fe361a7cc120ebeb2b34cb431f6f
ach-ber <77842805+ach-ber@users.noreply.github.com>
2022-03-28 14:33:53 +0200

    Update Thing.js

--- models/Thing.js
+++ models/Thing.js
@@ -18,8 +18,7 @@ const pool = new Pool({
     user: "npqvthisxioakn",
     host:"ec2-18-215-96-22.compute-1.amazonaws.com",
     database:"dda825ab0jjrog",
-    password:"de35b8f8463439c71326de2a7cc1dc16691ecbcaf3e4d13d0e3244f2e5ccbb5f",
-    port:5432,
+    password:"de35b8f8463439c71326de2a7cc1dc16691ecbcaf3e4d13d0e3244f2e5ccbb5f"
 });
 
 module.exports = pool;
